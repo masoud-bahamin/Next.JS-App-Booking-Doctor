9b04b057bf2e6ecbe57b87d598c84541dd18a39d
Masoud bahamin <Bahaminwp@gmail.com>
2023-12-07 14:21:32 +0300

    test

--- app/components/modules/Header/Navbar.tsx
+++ app/components/modules/Header/Navbar.tsx
@@ -5,7 +5,7 @@ export default function Navbar() {
     return (
         <ul className='flex gap-6 text-sm font-mediom'>
             <li className='hover:text-prim transition-all duration-500'><Link href={"/"}>Home</Link></li>
-            <li className='hover:text-prim transition-all duration-500'><Link href={"/"}>About Us</Link></li>
+            <li className='hover:text-prim transition-all duration-500'><Link href={"/"}>About</Link></li>
             <li className='hover:text-prim transition-all duration-500'><Link href={"/"}>Specialities</Link></li>
             <li className='hover:text-prim transition-all duration-500'><Link href={"/"}>Doctors</Link></li>
             <li className='hover:text-prim transition-all duration-500'><Link href={"/"}>Blog</Link></li>
