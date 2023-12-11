6bcc951728c94500f335ceb965631e12ab4a64aa
Masoud bahamin <Bahaminwp@gmail.com>
2023-12-12 00:32:29 +0300

    test path image

--- app/api/images/upload/route.ts
+++ app/api/images/upload/route.ts
@@ -20,7 +20,7 @@ export async function POST(req: any) {
 
     try {
         await writeFile(
-            path.join("/uploads/" + filename),
+            path.join(process.cwd(), "public/uploads/" + filename),
             buffer
         )
         const image = await imageModel.create({ filename, userId })
