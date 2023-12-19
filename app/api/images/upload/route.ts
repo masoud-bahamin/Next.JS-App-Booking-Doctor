fec6c91e49b2ef16c4206e53c13bdf9a125826c3
Masoud bahamin <Bahaminwp@gmail.com>
2023-12-19 14:18:14 +0300

    change save image in images route

--- app/api/images/upload/route.ts
+++ app/api/images/upload/route.ts
@@ -3,6 +3,7 @@ import connectToDb from "@/utils/db";
 import { writeFile } from "fs/promises";
 import { NextResponse } from "next/server";
 import path from "path"
+import fs from "fs"
 
 export async function POST(req: any) {
 
@@ -15,14 +16,21 @@ export async function POST(req: any) {
 
     if (!file) return NextResponse.json({ resulte: false, message: "image not found , error" })
 
-    const buffer = Buffer.from(await file.arrayBuffer());
+    // const buffer = Buffer.from(await file.arrayBuffer());
     const filename = Date.now() + file.name.replaceAll(" ", "_");
 
     try {
-        await writeFile(
-            path.join(process.cwd(), "public/uploads/" + filename),
-            buffer
-        )
+        const stream = fs.createWriteStream(`public/uploads/${filename}`)
+        const bufferdImage = await file.arrayBuffer()
+        stream.write(Buffer.from(bufferdImage) , (error) => {
+            if (error)
+            throw new Error (' saving image failed mas :((')
+        })
+
+        // await writeFile(
+        //     path.join(process.cwd(), "public/uploads/" + filename),
+        //     buffer
+        // )
         const image = await imageModel.create({ filename, userId })
         if (image) {
             return NextResponse.json({ resulte: true, message: "image upload successfully" }, {
